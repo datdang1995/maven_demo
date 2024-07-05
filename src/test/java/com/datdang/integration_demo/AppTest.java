b02de85710152e6e16911b6cfa4b4094641d425b
DatDang <tddang@linagora.com>
2024-07-05 17:08:25 +0700

    Add first test

--- src/test/java/com/datdang/integration_demo/AppTest.java
+++ src/test/java/com/datdang/integration_demo/AppTest.java
@@ -1,38 +1,43 @@
 package com.datdang.integration_demo;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import java.time.Duration;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AppTest {
+    WebDriver driver;
+
+    @Before
+    public void setup() {
+        driver = new ChromeDriver();
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    public void doSomething() {
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+        driver.getTitle();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+
+        WebElement textBox = driver.findElement(By.name("my-text"));
+        WebElement submitButton = driver.findElement(By.cssSelector("button"));
+
+        textBox.sendKeys("Selenium");
+        submitButton.click();
+
+        WebElement message = driver.findElement(By.id("message"));
+        message.getText();
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @After
+    public void teardown() {
+        driver.quit();
     }
 }
