6d68a57fb762dc6b5f0ccc52b9470def6faff428
DatDang <tddang@linagora.com>
2024-07-08 14:18:22 +0700

    update test

--- src/test/java/com/datdang/integration_demo/AppTest.java
+++ src/test/java/com/datdang/integration_demo/AppTest.java
@@ -9,13 +9,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class AppTest {
     WebDriver driver;
 
     @Before
     public void setup() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        driver = new ChromeDriver(options);
     }
 
     @Test
